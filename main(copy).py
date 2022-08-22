546593eac8847e56bbb10c5cd3e65cf9a7eb41ee
Hoseop.Lee <lhseop0710@gmail.com>
2022-08-22 23:02:00 +0900

    json data generation success

--- main(copy).py
+++ main(copy).py
@@ -8,12 +8,14 @@ from PyQt5 import QtCore, QtWidgets, QtWebEngineWidgets
 from PyQt5.QtWebEngineWidgets import QWebEnginePage
 from PyQt5.QtWebChannel import QWebChannel
 from PyQt5.QtCore import QObject, Qt
-import re
+from panoramic360 import panoramic360
 from secondwindow import secondwindow
 import json
+import cv2
 import beep
 import shutil
 from collections import OrderedDict
+import utils
 
 form_class = uic.loadUiType("SVS_Viewer.ui")[0]
 
@@ -22,22 +24,20 @@ class Handler(QObject):
         super(Handler, self).__init__(*args, **kwargs)
 
 class WebEnginePage(QWebEnginePage):
-    point = []
     def __init__(self, *args, **kwargs):
         super(WebEnginePage, self).__init__(*args, **kwargs)
 
     def javaScriptConsoleMessage(self, level, message, lineNumber, sourceId):
-        point = re.sub('|"|x|y|z|:|{|}', '', message)  #message 출력부 불필요한 문자열 제거
-        point = point.split(',')
-        self.point.clear()                              #리스트 초기화
-        self.point.append(point)                        #출력되는 좌표값 리스트에 추가
-        print(point)
-
+        l = message.split(",")
+        self.obj = l
+        print ('JS: %s line %d: %s' % (sourceId, lineNumber, message))
+        print("WebEnginePage Console: %s" %message)
+        return message
 
 class WindowClass(QMainWindow, form_class) :
     count_load_image = 0
     count_save_image = 0
-    image_directory_path = "/Users/leehoseop/PycharmProjects/pano_dataset/sejong_gumgang_bridge/"
+    directory_path = "/Users/leehoseop/Desktop/img_data/360_image/"
     file_name = "DJI_"
     ext = ".JPG"
     images_path = []
@@ -60,6 +60,18 @@ class WindowClass(QMainWindow, form_class) :
         self.image_Label.setStyleSheet("background-image : url(%s)" %self.background_picture)
         self.image_Label.setAutoFillBackground(True)
 
+        # self.setCentralWidget(self.central_widget)
+        # self.vertical.addWidget(QTextEdit())
+        # self.vertical.addWidget(QTextEdit())
+
+
+
+
+        ##
+        # self.widget_pano = QtWidgets.QWidget(self.centralwidget)
+        # self.widget_List.append(self.widget_pano)
+        # self.widget_pano.setObjectName("widget_youtube")
+
         # Set up backend communication via web channel
         self.handler = Handler()
         self.channel = QWebChannel()
@@ -74,21 +86,26 @@ class WindowClass(QMainWindow, form_class) :
         # Use a webengine view
         self.webview = QtWebEngineWidgets.QWebEngineView()
         self.webview.setPage(self.page)
+        # self.setCentralWidget(self.webview)
         self.webview.setWindowFlags(Qt.WindowStaysOnTopHint)  #| Qt.FramelessWindowHint
         self.webview.setGeometry(15, 12, 1200, 675)
+        self.webview.setFixedSize(1200, 675)
         self.webview.load(self.url)
         self.webview_layout.addWidget(self.webview)
+        # self.horizontal.addLayout(self.vertical)
+        # self.webview.show()
+        ##
 
         self.btn_loadFromFile.clicked.connect(self.File_Dialog)
         # self.pushbutton.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
         self.btn_Delete_List.clicked.connect(self.Clear_File_List)
         self.listWidget.itemSelectionChanged.connect(self.File_list_itemSelectionChange)
-        self.btn_svs.clicked.connect(self.loadImageFromFile)
+        self.btn_SVS.clicked.connect(self.loadImageFromFile)
         self.btn_Next.clicked.connect(self.NextImageFromFile)
         self.btn_Previous.clicked.connect(self.PreviousImageFromFile)
-        self.btn_Image_Data_Generator.clicked.connect(self.Image_Labling)
-        self.save_btn.clicked.connect(self.save_json_file)
+        self.btn_Image_Data_Generator.clicked.connect(self.Image_Labling)  #OpenCV창으로 넘어가도록 함
         self.listWidget.itemClicked.connect(self.Clicked_list_item)
+
 
     def File_Dialog(self):
         exts = ('.jpg', 'png', '.JPG', '.PNG')
@@ -122,10 +139,10 @@ class WindowClass(QMainWindow, form_class) :
             self.file_path_label.setText(str(path[0]) + "/" + item.text())
 
     def loadImageFromFile(self) :
-        clicked_points = WebEnginePage.point
-        self.point_x_label.setText(clicked_points[0][0])
-        self.point_y_label.setText(clicked_points[0][1])
-        self.point_z_label.setText(clicked_points[0][2])
+        # self.hide()             #메인윈도우 숨김
+        self.second = panoramic360(self.directory_path+self.file_name+str(self.count_load_image).zfill(4)+self.ext)  # 두번째창 생성
+        self.second.exec()  # 두번째창 닫을때까지 기다림
+        self.show()
 
     #클릭한 아이템을 라벨에 보여주도록 함
     def Clicked_list_item(self, item):
@@ -166,6 +183,15 @@ class WindowClass(QMainWindow, form_class) :
         else :
             self.listWidget.setCurrentRow(0)
 
+
+        # self.count_load_image -= 1
+        # #QPixmap 객체 생성 후 이미지 파일을 이용하여 QPixmap에 사진 데이터 Load하고, Label을 이용하여 화면에 표시
+        # self.qPixmapFileVar = QPixmap()
+        # self.qPixmapFileVar.load(self.directory_path+self.file_name+str(self.count_load_image).zfill(4)+self.ext)
+        # self.qPixmapFileVar = self.qPixmapFileVar.scaledToWidth(1200)
+        # self.image_Label.setPixmap(self.qPixmapFileVar)
+
+
     def dragEnterEvent(self, event):            #드래그 앤 드랍을 구현
             if event.mimeData().hasUrls():
                 event.accept()
@@ -177,13 +203,48 @@ class WindowClass(QMainWindow, form_class) :
             for f in files:
                 print(f)
 
+    clicked_points = []
+    blue, red = (255, 0, 0), (0, 0, 255)  # 색상 값
+
+    def onMouse(self, event, x, y, flags, param):  # 마우스 이벤트 핸들 함수  ---①
+        global isDragging, x0, y0, img  # 전역변수 참조
+        global x_point, y_point, w_point, h_point
+        global img_draw
+        if event == cv2.EVENT_LBUTTONDOWN:  # 왼쪽 마우스 버튼 다운, 드래그 시작 ---②
+            self.clicked_points.append((x,y))
+        elif event == cv2.EVENT_LBUTTONUP:  # 왼쪽 마우스 버튼 업 ---④
+            img_draw = img.copy()  # 선택 영역에 사각형 그림을 표시할 이미지 복제
+            for point in self.clicked_points:
+            # 선택 영역에 빨간 사각형 표시
+                cv2.circle(img_draw, (point[0], point[1]), 2, self.red, thickness = -1)
+                cv2.imshow('img_draw', img_draw)  # 빨간 사각형 그려진 이미지 화면 출력
+                # if self.w > 0 and self.h > 0:  # 폭과 높이가 양수이면 드래그 방향이 옳음 ---⑤
+                #     img_draw = img.copy()  # 선택 영역에 사각형 그림을 표시할 이미지 복제
+                #     # 선택 영역에 빨간 사각형 표시
+                #     cv2.circle(img_draw, (self.clicked_points[0], 3, self.red, 2)
+                #     cv2.imshow('img_draw', img_draw)  # 빨간 사각형 그려진 이미지 화면 출력
+                # else:
+                #     #cv2.imshow('img', img)  # 드래그 방향이 잘못된 경우 사각형 그림ㅇㅣ 없는 원본 이미지 출력
+                #     print("좌측 상단에서 우측 하단으로 영역을 드래그 하세요.")
+        # elif event == cv2.EVENT_RBUTTONDOWN:        #cv2.EVENT_RBUTTONDBCKL은 macOS Montrey, openCV4.5 python3.7에서 지원되지 않음
+        #     print("destroyWindow")
+        #     cv2.destroyWindow('img_draw')
+
     def Image_Labling(self):            #파일의 경로를 받아올 수 있도록 함
-        global i
-        global imag_path_dir
-        global file_list
-        imag_path_dir = QFileDialog.getExistingDirectory(self, 'Open File')
-        file_list = os.listdir(imag_path_dir)
+        global img  #전역변수 참조
+        global clicked_points #전역변수 참조
+        global img_draw
+        Labeled_data_path = self.directory_path #라벨링한 이미지의 경로를 지정
+        path_dir = QFileDialog.getExistingDirectory(self, 'Open File')
+        file_list = os.listdir(path_dir)
         file_list.sort()
+        # 맥OS에서는 list를 자동적으로 정렬해주지 않으므로 list.sort()
+        # 기본적으로 리스트를 오름차순으로 정렬 해줌
+        #fname = QFileDialog.getOpenFileName(self)
+        #path = QFileDialog.getExistingDirectory(self, 'Open File') + '/'   #경로는 리스트로 출력됨 사용할때는 반드시 뒤에 []에 해당 인덱스를 붙여줘야함
+        isDragging = False  # 마우스 드래그 상태 저장
+        x0, y0, w, h = -1, -1, -1, -1  # 영역 선택 좌표 저장
+        blue, red = (255, 0, 0), (0, 0, 255)  # 색상 값
 
         if not file_list:
             print("img_load_fail")
@@ -191,52 +252,90 @@ class WindowClass(QMainWindow, form_class) :
 
         i = 1
         while i < len(file_list):
-            img_name = imag_path_dir + '/' + file_list[i]
+            img_name = path_dir + "/" + file_list[i]
+            print(path_dir)
             print(file_list[i])
             print(img_name)
+            img = cv2.imread(img_name, cv2.IMREAD_COLOR)
+            cv2.imshow('img_data', img)
+            key = cv2.waitKey(0)
             if i > len(file_list):
                 msg = QMessageBox()
                 msg.setWindowTitle('Message')
                 msg.setText("No more Images, Press OK")
                 break
 
-            self.second = secondwindow()    #두번째창 생성
-            self.second.exec()          #두번째창 닫을때까지 기다림
-            # self.show()  # 두번쨰창 닫으면 다시 메인윈도우
+            if key == 110:  # 'n'을 누르면 다음사진
+                print('Next_img:   ', key)
 
+                i += 1
+                cv2.imshow("index number" + str(i) + self.ext, img)
+                cv2.destroyWindow("index number" + str(i-1)  + self.ext)
 
-    def save_json_file(self):
-        global i
-        global imag_path_dir
-        global file_list
+            elif key == 98:  # 'b'를 누르면 이전사진
+                print('Previous_img:   ', key)
+                i -= 1
+                cv2.imshow("index number" + str(i) + self.ext, img)
+                cv2.destroyWindow("index number" + str(i+1)  + self.ext)
 
-        clicked_points = WebEnginePage.point
-        # image_name =
-        selected_object = self.second.second_text_item
-        object_number = self.second.second_text_index
-        production = "skyviewersolution"
+            elif key ==  32:  # 'spacebar'를 누르면 라벨링
+                print('Mouse_Event')
+                cv2.imshow('%s' %(str(i)), img)
+                cv2.setMouseCallback('img_draw', self.onMouse)  # 마우스 이벤트 소환 ---⑧
 
-        # json파일에 정보를 넣어준다.
-        file_data = OrderedDict()
-        file_data["version"] = "1.0.0"
-        file_data["filename"] = "%s" % file_list[i]
-        file_data["shapes"] = {'label': '%s' % selected_object,
-                               'production_company': "%s" % production,
-                               'point_x': [clicked_points[0][0]],
-                               'point_y': [clicked_points[0][1]],
-                               'point_z': [clicked_points[0][2]],
-                               }
-        file_data["number"] = "%s" % object_number
+                self.hide()         #메인윈도우 숨김
+                self.second = secondwindow()    #두번째창 생성
+                self.second.exec()          #두번째창 닫을때까지 기다림
+                # self.show()  # 두번쨰창 닫으면 다시 메인윈도우
 
-        print(json.dumps(file_data, ensure_ascii=False, indent="\t"))
+                query = self.second.second_text
+                selected_object = query
+                object_number = None
+                production = None
 
-        with open(imag_path_dir + file_list[i] + '.json', 'w', encoding="utf-8") as make_file:
-            json.dump(file_data, make_file, ensure_ascii=False, indent="\t")
-        # shutil.copy(img_name, Labeled_data_path)
-        i += 1
-        #
-        # def home(self):
-        #     self.show()             #두번째창 닫으면 다시 첫 번째 창 보여 짐
+                if selected_object == "information center":     #안내소
+                    object_number = "1"
+                elif selected_object == "ticket box":           #매표소
+                    object_number = "2"
+                elif selected_object == "information board":    #안내판
+                    object_number = "3"
+                elif selected_object == "life saving goods":    #인명구호용품
+                    object_number = "4"
+                elif selected_object == "defibrillators":       #심장제세동기
+                    object_number = "5"
+                elif selected_object == "toilet":               #화장실
+                    object_number = "6"
+                elif selected_object == "tourist attraction":   #관광명소
+                    object_number = "7"
+                elif selected_object == "building":             #건물
+                    object_number = "8"
+                elif selected_object == "store":               #상점
+                    object_number = "9"
+
+                #json파일에 정보를 넣어준다.
+                file_data = OrderedDict()
+                file_data["version"] = "1.0.0"
+                file_data["filename"] = "%s" %selected_object
+                file_data["shapes"] = {'label': '%s' % str(query),
+                                       'production_company': "%s" %production,
+                                       'points': [clicked_points]}
+                file_data["number"] = "%s" %object_number
+
+                print(json.dumps(file_data, ensure_ascii=False, indent="\t"))
+
+            elif key == 109: # 'm'을 누르면 json파일 저장 및 이미지 복사
+                with open(self.directory_path + file_list[i] + '.json', 'w', encoding="utf-8") as make_file:
+                    json.dump(file_data, make_file, ensure_ascii=False, indent="\t")
+                shutil.copy(self.file_name, self.directory_path)
+                beep.beep(sound=1)
+                cv2.waitKey()
+                cv2.destroyWindow('img_draw')
+
+            elif key == 27:  # 'ESC'를 누르면 종료
+                print('Exit_image_viewer')
+                cv2.destroyAllWindows()
+                self.show()             #두번째창 닫으면 다시 첫 번째 창 보여 짐
+                break
 
 
 
--- secondwindow.py
+++ secondwindow.py
@@ -18,9 +18,9 @@ class secondwindow(QDialog, QWidget, form_secondwindow):
         self.Delete_btn.clicked.connect(self.Delete_Object)
         self.Clear_btn.clicked.connect(self.Clear_Object)
         self.Object_list.itemSelectionChanged.connect(self.Object_list_itemSelectionChange)
-        self.Save_btn.clicked.connect(self.save_labeling_information)
+        self.Adjust_btn.clicked.connect(self.adjust_labeling_information)
         # self.close_btn.clicked.connect(self.close)
-        self.home_btn.clicked.connect(self.home)
+        # self.home_btn.clicked.connect(self.home)
 
     def initUI(self):
         self.setWindowTitle("SVS_DATA_CREATOR" + " / Version Beta")
@@ -42,27 +42,27 @@ class secondwindow(QDialog, QWidget, form_secondwindow):
 
     def Object_list_itemSelectionChange(self):
         item = self.Object_list.currentItem()
+        value = self.Object_list.currentRow()
         if (item == None):
             self.item_label.setText("[selection]")
+            self.index_lable.setText("[selection]")
             self.Delete_btn.setEnabled(False)
         else:
             self.item_label.setText(item.text())
+            self.index_label.setText(str(value))
             self.Delete_btn.setEnabled(True)
 
-    def save_labeling_information(self):
-        self.second_text = self.item_label.text()
+    def adjust_labeling_information(self):
+        self.second_text_item = self.item_label.text()
+        self.second_text_index = self.index_label.text()
+
 
     def close(self):
         self.close()  # 메인윈도우 숨김
-        # self.seconde = secondwindow()  # 두번째창 생성
-        # self.seconde.exec()  # 두번째창 닫을때까지 기다림
-        # self.hide()
-        cv2.destroyAllWindows()
 
-    def home(self):
-        self.close()  # 메인윈도우 숨김
+    # def home(self):
+    #     self.close()  # 메인윈도우 숨김
     #     self.main = form_class()  # 두번째창 생성
     #     self.main.exec()  # 두번째창 닫을때까지 기다림
     #     self.hide()
-        cv2.destroyAllWindows()
 
